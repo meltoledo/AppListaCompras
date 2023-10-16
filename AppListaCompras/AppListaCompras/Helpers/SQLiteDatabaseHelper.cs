ca68db85eb8b34d1bdb7ee682aee70cfbc913913
Melissa Toledo <melissatoledo87@gmail.com>
2023-10-15 21:40:38 -0300

    Adicionar arquivos de projeto.

---
+++ AppListaCompras/AppListaCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppListaCompras.Models;
+using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace AppListaCompras.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public  SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+
+        }
+        public  Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE id=?";
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * FROM Produto WHERE Descricao LIKE '%" + q + "%'";
+            return _conn.QueryAsync<Produto>(sql);
+
+        }
+    }
+}
---
+++ AppListaCompras/AppListaCompras/Views/NovoProduto.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppListaCompras.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NovoProduto : ContentPage
+    {
+        public NovoProduto()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
